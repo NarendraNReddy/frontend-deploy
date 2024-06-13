b243f0730155ac764debd7007a9e036503db77b7
NarendraReddy <narendra.nya@gmail.com>
2024-06-12 21:39:29 -0700

    frontend-deploy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
     bucket = "daws78s-nnr6"
-    key    = "expense-infra-frontend_pr3344444"
+    key    = "expense-infra-frontend_deploy"
     region = "us-east-1"
     dynamodb_table = "daws78s-nnr6"
   }
